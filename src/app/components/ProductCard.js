9460fc8aa74eb6cb474679af0ead512cc7bafd3c
Manit Cholpinyo <manit.cholpinyo@krungsri.com>
2024-06-13 23:31:06 +0700

    test hello

---
+++ src/app/components/ProductCard.js
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <dev>
+        <button onClick={() => console.log("Add")}> Add to cart</button>
+    </dev>
+  )
+}
+
+export default ProductCard
